3877bcf863aac1cbcea272961b115b74d4ff7eb8
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-06-11 23:57:38 +0300

    fix wordpress error

--- 0x17-web_stack_debugging_3 /0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3 /0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-#Fix error in Apache
+# Fix typo in WordPress settings file
+exec { 'Fix typo in filename':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
