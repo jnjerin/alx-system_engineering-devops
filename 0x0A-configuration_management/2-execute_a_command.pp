765261a0f4616bfa89fbc6f7928f9ab1b221a6f4
jnjerin <njerijoan24@gmail.com>
2024-05-03 19:26:07 +0300

    Add provider to kill a process  code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Kill a process named killmenow
 exec { 'killmenow':
-  command => '/usr/bin/pkill killmenow',
-  path => '/bin:/usr/bin',
-  refreshonly => true,
+  command => 'pkill killmenow',
+  provider => shell,
 }
 
