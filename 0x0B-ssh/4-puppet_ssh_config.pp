41ba519f8429bc6bf1c151ea191963a6470d90ba
jnjerin <njerijoan24@gmail.com>
2024-05-08 11:00:46 +0300

    config ssh with puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,10 @@
-
+file { '~/.ssh/config':
+  ensure  => present,
+  mode    => '0600',
+  content => '
+    Host 52.3.241.110
+            IdentityFile ~/.ssh/school
+            PreferredAuthentications publickey
+            PasswordAuthentication no
+  ',
+}
