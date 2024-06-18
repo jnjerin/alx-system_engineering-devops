809b0d31fcf05c86d59d1e43264bf8c483ef0267
jnjerin <njerijoan24@gmail.com>
2024-06-19 00:10:31 +0300

    update fucntion logic

--- 0x16-api_advanced /1-top_ten.py
+++ 0x16-api_advanced /1-top_ten.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
-"""
-
+'''
+ Prints the titles of the first 10 hot
+ posts listed for a given subreddit.
+'''
 import requests
 
 
 def top_ten(subreddit):
-    """Request top ten hot posts of subreddit
-    from Reddit API
-    """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-jnjerin'
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    '''
+    Top 10 posts  in subreddit
+    '''
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = 'reddit_user'
 
-    # custom user-agent avoids request limit
     headers = {'User-Agent': user_agent}
 
-    r = requests.get(url, headers=headers, allow_redirects=False)
+    req = requests.get(url, headers=headers, allow_redirects=False)
 
-    if r.status_code != 200:
+    if req.status_code != 200:
         print('None')
     else:
-        # load response unit from json
-        data = r.json()['data']
-        # extract list of pages
-        posts = data['children']
-        for post in posts:
-            print(post['data']['title'])
+        data = req.json()['data']
+        post_list = data['children']
+
+        for posts in post_list[0:10]:
+            print(posts['data']['title'])
